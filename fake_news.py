a5ccb58e08be0d9995d0507b4b7c71234ac83d0f
Saffat Akanda <jadenandyubel@hotmail.com>
2018-05-19 12:18:58 +1000

    initial commit- added python files

---
+++ fake_news.py
@@ -0,0 +1,28 @@
+# Fake News Challenge Attempt using RNN
+# Saffat Shams Akanda, z5061498 @ UNSW
+
+import tensorflow
+import tensorboard
+import numpy
+import os
+import rnn_model
+
+
+def read_embeddings():
+    pass
+
+
+def read_input_file():
+    pass
+
+
+def preprocess_file():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ rnn_model.py
@@ -0,0 +1,11 @@
+# Fake News Challenge Attempt using RNN
+# Saffat Shams Akanda, z5061498 @ UNSW
+
+import tensorflow
+import tensorboard
+import numpy
+import os
+
+
+class recurrentModel():
+    pass
