9838a13bd010382c0dd058f6a15efb6ab5bcebae
Saffat Akanda <jadenandyubel@hotmail.com>
2018-05-21 00:35:40 +1000

    Added code to read in glove embeddings

--- fake_news.py
+++ fake_news.py
@@ -3,13 +3,30 @@
 
 import tensorflow
 import tensorboard
-import numpy
+import numpy as np
 import os
 import rnn_model
 
+MAX_SEQ_LEN = 2000
+EMBEDDING_DIMENSIONS = 300
+VALIDATION_SPLIT = 0.2
+
 
 def read_embeddings():
-    pass
+    if os.path.exists("glove.840B.300d"):
+        file = open("glove.840B.300d", "r", encoding="utf-8")
+    else:
+        raise Exception("No Glove File found in directory, requires glove.840B.300d")
+
+    embeddings_index = dict()
+    embeddings_index["UNK"] = 0
+    for line in file:
+        currline = line.split()
+        word = currline[0]
+        embeddings_index[word] = np.asarray(currline[1:], dtype='float32')
+
+    file.close()
+    return embeddings_index
 
 
 def read_input_file():
