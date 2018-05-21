ed2c3bfbed1ac957a09e35eca0a8173dac1cf5ce
Saffat Akanda <jadenandyubel@hotmail.com>
2018-05-21 16:02:28 +1000

    Changed to 200 Dimension embeddings, Tested that embeddings and text are loaded correctly.

--- fake_news.py
+++ fake_news.py
@@ -3,23 +3,26 @@
 
 import keras
 import tensorboard
+import pandas as pd
 import numpy as np
 import os
 import rnn_model
 
-MAX_SEQ_LEN = 2000
-EMBEDDING_DIMENSIONS = 300
+MAX_SEQ_LEN = 250
+EMBEDDING_DIMENSIONS = 200
 VALIDATION_SPLIT = 0.2
+
+TRAINING_DIR = "training_data/"
+EMBEDDING_NAME = "glove.6B.200d.txt"
 
 
 def read_embeddings():
-    if os.path.exists("glove.840B.300d"):
-        file = open("glove.840B.300d", "r", encoding="utf-8")
+    if os.path.exists(EMBEDDING_NAME):
+        file = open(EMBEDDING_NAME, "r", encoding="utf-8")
     else:
-        raise Exception("No Glove File found in directory, requires glove.840B.300d")
+        raise Exception("No Glove File found in directory, requires " + EMBEDDING_NAME)
 
     embeddings_index = dict()
-    embeddings_index["UNK"] = 0
     for line in file:
         currline = line.split()
         word = currline[0]
@@ -30,15 +33,27 @@ def read_embeddings():
 
 
 def read_input_files():
-    pass
+    labels = []
+    text = []
+    with open(TRAINING_DIR + "train_stances.csv", "r", encoding="utf-8") as file:
+        train_bodies = pd.read_csv(TRAINING_DIR + "train_bodies.csv", index_col="Body ID")
 
+        next(file)                          # First line has header info
+        for line in file:
+            values = line.rsplit(",", 2)
+            labels.append(values[2])
+            text.append(values[0] + " " + train_bodies.at[int(values[1]), "articleBody"])
 
-def preprocess_file():
-    pass
+    return text, labels
 
 
 def main():
-    pass
+    print("Reading in embeddings")
+    embeddings_index = read_embeddings()
+    print("Reading in input files now")
+    train_text, train_labels = read_input_files()
+
+    print("I CAN READ")
 
 
 if __name__ == "__main__":
